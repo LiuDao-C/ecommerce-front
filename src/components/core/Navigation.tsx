469061c1bcbbbc681a88f647a5e5a7aaee86efab
六道 <592922218@qq.com>
2023-01-08 14:46:18 +0800

    R8 msg: 处理登录结果

--- src/components/core/Navigation.tsx
+++ src/components/core/Navigation.tsx
@@ -2,6 +2,8 @@ import { Menu } from "antd"
 import { RouterState } from "connected-react-router"
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
+import { isAuth } from "../../helpers/auth"
+import { Jwt } from "../../store/models/auth"
 import { AppState } from "../../store/reducers"
 
 function useActive(currentPath: string, path: string): string {
@@ -15,6 +17,19 @@ const Navigation = () => {
     const isShop = useActive(pathname, "/shop")
     const isSignin = useActive(pathname, "/signin")
     const isSignup = useActive(pathname, "/signup")
+    const isDashboard = useActive(pathname, getDashboardUrl())
+
+    function getDashboardUrl() {
+        let url = '/user/dashboard'
+        if(isAuth()) {
+            const { user: { role} } = isAuth() as Jwt
+            if(role === 1) {
+                url = "/admin/dashboard"
+            }
+        }
+        return url
+    }
+
     return <Menu mode="horizontal" selectable={false}>
         <Menu.Item className={isHome}>
             <Link to="/">首页</Link>
@@ -22,12 +37,27 @@ const Navigation = () => {
         <Menu.Item className={isShop}>
             <Link to="/shop">商城</Link>
         </Menu.Item>
-        <Menu.Item className={isSignin}>
-            <Link to="/signin">登录</Link>
-        </Menu.Item>
-        <Menu.Item className={isSignup}>
-            <Link to="/signup">注册</Link>
-        </Menu.Item>
+        {
+            !isAuth() && (
+                <>
+                    <Menu.Item className={isSignin}>
+                        <Link to="/signin">登录</Link>
+                    </Menu.Item>
+                    <Menu.Item className={isSignup}>
+                        <Link to="/signup">注册</Link>
+                    </Menu.Item>
+                </>
+            )
+        }
+        {
+            isAuth() && (
+                <>
+                    <Menu.Item className={isDashboard}>
+                        <Link to={getDashboardUrl()}>dashboard</Link>
+                    </Menu.Item>
+                </>
+            )
+        }
     </Menu>
 }
 
--- src/components/core/Signin.tsx
+++ src/components/core/Signin.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Layout from './Layout'
-import { Form, Input, Button } from "antd"
+import { Form, Input, Button, Result } from "antd"
 import { signin, SigninPayload } from '../../store/actions/auth.action'
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { AppState } from '../../store/reducers'
+import { AuthState } from '../../store/reducers/auth.reducer'
+import { isAuth } from '../../helpers/auth'
+import { Jwt } from '../../store/models/auth'
+import { Redirect } from 'react-router-dom'
 
 const Signin = () => {
     // 获取 dispatch
@@ -10,8 +15,36 @@ const Signin = () => {
     const onFinish = (value: SigninPayload) => {
         dispatch(signin(value))
     }
+    // 1、获取登录结果
+    const auth = useSelector<AppState, AuthState>(state => state.auth)
+    // 2、登录失败 显示错误信息
+    const showError = () => {
+        if(auth.signin.loaded && !auth.signin.success) {
+            return <Result
+            status="warning"
+            title="登录失败"
+            subTitle={auth.signin.message}
+          />
+        }
+    }
+    // 3、登录成功 根据角色跳转到对应的管理页面
+    const redirectToDashboard = () => {
+        const auth = isAuth()
+        if(auth) {
+            const { user: { role } } = auth as Jwt
+            if(role === 0) {
+                // 注册用户
+                return <Redirect to="/user/dashboard" />
+            } else {
+                // 管理员
+                return <Redirect to="/admin/dashboard" />
+            }
+        }
+    }
+    // 4、处理导航连接 已登录 隐藏 【登录，注册】 显示 【dashboard】
+    
 
-    return <Layout title="登录" subTitle="">
+    const signinForm = () => (
         <Form onFinish={onFinish}>
             <Form.Item name="email" label="邮箱">
                 <Input />
@@ -25,6 +58,12 @@ const Signin = () => {
                 </Button>
             </Form.Item>
         </Form>
+    )
+
+    return <Layout title="登录" subTitle="">
+        {showError()}
+        {redirectToDashboard()}
+        {signinForm()}
     </Layout>
 }
 
